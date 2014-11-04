7df425a09856667f4d9ec21ffe67818415b3e1bd
Stephen Lauck <stephen.lauck@gmail.com>
2014-11-04 14:26:59 -0500

    use -f for creating key

--- recipes/create_org.rb
+++ recipes/create_org.rb
@@ -8,7 +8,7 @@ execute 'create organization' do
     chef-server-ctl org-create #{organization}
     #{organization_long_name}
     -a #{admin_username}
-    > #{organization_private_key_path}
+    -f #{organization_private_key_path}
   EOM
   not_if "chef-server-ctl org-list | grep -w #{organization}"
 end
