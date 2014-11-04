792d137139f9a2477ab7a81eb8dea6a821f401b1
Stephen Lauck <stephen.lauck@gmail.com>
2014-11-04 16:58:50 -0500

    remove local variables and use node attributes directly in resource

--- recipes/create_admin.rb
+++ recipes/create_admin.rb
@@ -1,18 +1,11 @@
-username          = node['chef-server-cluster']['admin']['username']
-firstname         = node['chef-server-cluster']['admin']['firstname']
-lastname          = node['chef-server-cluster']['admin']['lastname']
-email             = node['chef-server-cluster']['admin']['email']
-password          = node['chef-server-cluster']['admin']['password']
-private_key_path  = node['chef-server-cluster']['admin']['private_key_path']
-
 execute 'create admin' do
   command <<-EOM.gsub(/\s+/, ' ').strip!
-    chef-server-ctl user-create #{username}
-    #{firstname}
-    #{lastname}
-    #{email}
-    #{password}
-    -f #{private_key_path}
+    chef-server-ctl user-create #{node['chef-server-cluster']['admin']['username']}
+    #{node['chef-server-cluster']['admin']['firstname']}
+    #{node['chef-server-cluster']['admin']['lastname']}
+    #{node['chef-server-cluster']['admin']['email']}
+    #{node['chef-server-cluster']['admin']['password']}
+    -f #{node['chef-server-cluster']['admin']['private_key_path']}
   EOM
-  not_if "chef-server-ctl user-list | grep -w #{username}"
+  not_if "chef-server-ctl user-list | grep -w #{node['chef-server-cluster']['admin']['username']}"
 end
--- recipes/create_org.rb
+++ recipes/create_org.rb
@@ -1,14 +1,9 @@
-organization                  = node['chef-server-cluster']['organization']
-organization_long_name        = node['chef-server-cluster']['organization_long_name']
-admin_username                = node['chef-server-cluster']['admin']['username']
-organization_private_key_path = node['chef-server-cluster']['organization_private_key_path']
-
 execute 'create organization' do
   command <<-EOM.gsub(/\s+/, ' ').strip!
-    chef-server-ctl org-create #{organization}
-    #{organization_long_name}
-    -a #{admin_username}
-    -f #{organization_private_key_path}
+    chef-server-ctl org-create #{node['chef-server-cluster']['organization']}
+    #{node['chef-server-cluster']['organization_long_name']}
+    -a #{node['chef-server-cluster']['admin']['username']}
+    -f #{node['chef-server-cluster']['organization_private_key_path']}
   EOM
-  not_if "chef-server-ctl org-list | grep -w #{organization}"
+  not_if "chef-server-ctl org-list | grep -w #{node['chef-server-cluster']['organization']}"
 end
