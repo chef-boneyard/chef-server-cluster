d7b39d53586c630c6d554d8012f7e2bc978478fc
jtimberman <joshua@chef.io>
2015-02-04 22:59:06 -0700

    configuration for analytics 1.1

--- recipes/analytics.rb
+++ recipes/analytics.rb
@@ -19,6 +19,7 @@
 #
 # Maybe we'll use a data bag to store these
 node.default['chef-server-cluster']['role'] = 'analytics'
+analytics_fqdn = data_bag_item('chef_server', 'topology')['analytics_fqdn'] || node['ec2']['public_hostname']
 
 # We define these here instead of including the default recipe because
 # analytics doesn't actually need chef-server-core.
@@ -31,7 +32,7 @@ directory '/etc/opscode-analytics' do
 end
 
 file '/etc/opscode-analytics/opscode-analytics.rb' do
-  content "topology 'standalone'\nanalytics_fqdn '#{node['ec2']['public_hostname']}'"
+  content "topology 'standalone'\nanalytics_fqdn '#{analytics_fqdn}'"
   notifies :reconfigure, 'chef_server_ingredient[opscode-analytics]'
 end
 
