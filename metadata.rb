6ce381f2a7994fb9e70e712fb5e415c3cc3664b0
Tim Smith <tsmith84@gmail.com>
2015-09-10 21:40:39 -0700

    Add supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,8 @@ maintainer       'Chef Software, Inc.'
 maintainer_email 'ops@getchef.com'
 license          'Apache 2.0'
 description      'A Chef cookbook from CHEF for managing Chef Server clusters. Turtles.'
+source_url 'https://github.com/chef-cookbooks/chef-server-cluster' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/chef-server-cluster/issues' if respond_to?(:issues_url)
 version          '0.0.9'
 depends          'chef-server-ingredient'
 depends          'chef-vault'
