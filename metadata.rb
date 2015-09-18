298d6c02e92656468c3e6fd18f235b9559c6d8b1
Tim Smith <tsmith84@gmail.com>
2015-09-18 10:43:37 -0700

    Add long_description

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'ops@chef.io'
 license 'Apache 2.0'
 description 'A Chef cookbook from CHEF for managing Chef Server clusters. Turtles.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/chef-cookbooks/chef-server-cluster' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/chef-server-cluster/issues' if respond_to?(:issues_url)
 version '0.0.9'
